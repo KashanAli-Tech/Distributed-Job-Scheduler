19c4440ce7e9d1c93d541854d0a042317aaf2d79
KashanAli-Tech <off.kashanali@gmail.com>
2026-07-01 21:38:34 +0100

    Fixed error in system.py - Queue() to JobQueue()

--- app/core/system.py
+++ app/core/system.py
@@ -1,11 +1,11 @@
-from app.core.queue import Queue
+from app.core.queue import JobQueue
 from app.core.worker import Worker
 
 # central job storage (single source of truth)
 jobs = {}
 
 # shared queue
-queue = Queue()
+queue = JobQueue()
 
 # worker gets everything injected (NO imports back into system inside worker)
 worker = Worker(queue, jobs)
